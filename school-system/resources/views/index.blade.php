0bf3fef01e2b1cb30cbb684b6e727283f8d2fa0d
keerthikan <164475251+Sivakumarankeerthikan@users.noreply.github.com>
2025-12-18 09:26:22 +0530

    Add DataTables CSS to index view
    Included the DataTables CSS CDN link in the index.blade.php file for future table enhancements. Also added (but commented out) the jQuery and DataTables JS scripts, preparing for potential interactive table features.

--- school-system/resources/views/index.blade.php
+++ school-system/resources/views/index.blade.php
@@ -5,8 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" type="image/x-icon" href="{{ asset('home-button.png') }}">
+    {{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script> --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.3.5/css/dataTables.dataTables.css">
     <title>@yield('title')</title>
     <style>
     body {
@@ -77,5 +79,11 @@
     @yield('content');
   </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+  {{-- <script src="https://cdn.datatables.net/2.3.5/js/dataTables.min.js"></script>
+  <script>
+    $(document).ready(function () {
+      $('#mytable').DataTable();
+    });
+  </script> --}}
 </body>
 </html>
